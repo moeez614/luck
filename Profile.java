e12d10c3a4a4573d644d2b81db474e20482af1be
moeez614 <moeez7697@gmail.com>
2025-04-11 18:03:02 +0500

    Add files via upload

---
+++ Profile.java
@@ -0,0 +1,8 @@
+import java.applet.Applet;
+import java.io.IOException;
+public class Profile  {
+
+    protected void doGet( request, HttpServletResponse response) throws IOException{
+        response.setContentType()
+    }
+}
